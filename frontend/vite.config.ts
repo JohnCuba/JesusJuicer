5f115a5e48b7bdf730f17faa5556aa5da3884c10
JohnCuba <ekubetsky@gmail.com>
2025-04-26 11:11:04 +0700

    chore: rewrite frontend compressing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,30 +1,81 @@
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
+import { readdirSync, readFileSync, unlinkSync, writeFileSync, lstatSync } from 'node:fs'
+import { resolve, extname } from 'node:path'
+import { gzipSync } from 'node:zlib'
+import { styleText } from 'node:util'
+import { defineConfig, Plugin } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import gzipPlugin from 'rollup-plugin-gzip'
-import cleanPlugin from 'rollup-plugin-delete'
 import svgLoader from 'vite-svg-loader'
 import tailwindcss from '@tailwindcss/vite'
 
+const viteGzipPlugin = (): Plugin => {
+  const acceptedExt = ['.html', '.css', '.js', '.ico']
+  const niceBytes = (byteLength: string) => {
+    const units = ['b', 'kB', 'mB']
+    let l = 0,
+      n = parseInt(byteLength, 10) || 0
+
+    while (n >= 1024 && ++l) {
+      n = n / 1024
+    }
+
+    return n.toFixed(n < 10 && l > 0 ? 1 : 0) + ' ' + units[l]
+  }
+
+  return {
+    name: 'vite-compress-plugin',
+    apply: 'build',
+    enforce: 'post',
+    async closeBundle() {
+      console.info(styleText('blue', 'Start compressing'))
+      const timeStart = Date.now()
+      const outputLineGap = 4
+      let maxLine = 0
+      const results: [string, string][] = []
+
+      const files = readdirSync(this.environment.config.build.outDir, { recursive: true })
+      files.forEach((filePath) => {
+        // Check file extension
+        if (!acceptedExt.includes(extname(String(filePath)))) return
+        const absoluteFilePath = resolve(
+          process.cwd(),
+          this.environment.config.build.outDir,
+          filePath as string,
+        )
+        // Check is file
+        if (lstatSync(absoluteFilePath).isDirectory()) return
+        const file = readFileSync(absoluteFilePath)
+        const compressedFile = gzipSync(file)
+        unlinkSync(absoluteFilePath)
+        writeFileSync(absoluteFilePath + '.gz', compressedFile)
+
+        const outputFileName = this.environment.config.build.outDir + filePath + '.gz'
+        const outputFileSize = niceBytes(compressedFile.byteLength.toString())
+        const lineSize = outputFileName.length + outputFileSize.length
+        maxLine = lineSize > maxLine ? lineSize : maxLine
+        results.push([outputFileName, outputFileSize])
+      })
+
+      console.info(
+        results.reduce((output, [fileName, fileSize], index) => {
+          return (
+            output +
+            (index ? '\n' : '') +
+            fileName.padEnd(maxLine + outputLineGap - fileSize.length) +
+            fileSize
+          )
+        }, ''),
+      )
+
+      console.info(styleText('green', `✓ Compressed in ${Date.now() - timeStart}ms`))
+    },
+  }
+}
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    svgLoader(),
-    tailwindcss(),
-    gzipPlugin({
-      customCompression: (content: string | Buffer) => Buffer.from(Bun.gzipSync(content)),
-    }),
-    cleanPlugin({
-      targets: ['../data/public/**/*.html', '../data/public/**/*.js', '../data/public/**/*.css'],
-      force: true,
-      runOnce: true,
-      hook: 'closeBundle',
-      verbose: true,
-    }),
-  ],
+  plugins: [vue(), vueDevTools(), svgLoader(), tailwindcss(), viteGzipPlugin()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
@@ -38,5 +89,6 @@ export default defineConfig({
         chunkFileNames: (chunkInfo) => `assets/${chunkInfo.name.split('.')[0]}-[hash].js`,
       },
     },
+    reportCompressedSize: false,
   },
 })
