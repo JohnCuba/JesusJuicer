0c9d6921b7aaad1415a225d8ec4bf608632573cc
JohnCuba <ekubetsky@gmail.com>
2025-05-10 11:27:19 +0300

    fix: reset progress

--- frontend/src/data/config_upload.ts
+++ frontend/src/data/config_upload.ts
@@ -7,13 +7,16 @@ type useUploadFileArgs = {
 export const useUploadFile = ({ url = '/' }: useUploadFileArgs) => {
   const progress = ref<number>(0)
 
+  const resetProgress = () => (progress.value = 0)
+
   const upload = async (data: File) => {
     const xhr = new XMLHttpRequest()
     xhr.open('POST', url, true)
 
-    xhr.onload = () => {
-      progress.value = 0
-    }
+    xhr.onloadend = resetProgress
+    xhr.onload = resetProgress
+    xhr.onabort = resetProgress
+    xhr.onerror = resetProgress
 
     xhr.upload.onprogress = (ev) => {
       progress.value = Math.trunc((ev.loaded / ev.total) * 100)
